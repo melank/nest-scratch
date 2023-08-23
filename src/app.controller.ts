f70723937582c9f7dc397435483e4ec8b4eecfc0
melank <masaki.vr@gmail.com>
2023-08-23 23:31:00 +0900

    nest initialize.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,12 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
-@Controller("/app")
+@Controller()
 export class AppController {
-  @Get("/hi")
-  getRootRoute() {
-    return "hi there!";
-  }
+  constructor(private readonly appService: AppService) {}
 
-  @Get("/bye")
-  getByThere() {
-    return "bye there!";
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
