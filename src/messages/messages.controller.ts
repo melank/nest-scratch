ae01912558ef80dd66b257f788aaa1da0288a827
melank <masaki.vr@gmail.com>
2023-08-26 16:59:38 +0900

    implements codes of using service in controller.

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,29 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // 一時的にDIを使わずに依存関係を作り実装
+    // 後で削除する
+    this.messagesService = new MessagesService();
+  }
+
   @Get('/')
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post('/')
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   GetMessage(@Param() id: string) {
-    console.log(id);
+    return this.messagesService.findOne(id);
   }
 }
